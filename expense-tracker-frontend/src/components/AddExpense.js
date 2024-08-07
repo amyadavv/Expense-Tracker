6bff7989234483c85fc790dfa1635bd932b87783
amyadavv <116055245+amyadavv@users.noreply.github.com>
2024-08-07 20:17:34 +0530

    Update AddExpense.js
    added backend port

--- expense-tracker-frontend/src/components/AddExpense.js
+++ expense-tracker-frontend/src/components/AddExpense.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://expense-tracker-backend-km1u.onrender.com';
 
 const AddExpense = ({ setExpenses, setTotal }) => {
     const [description, setDescription] = useState('');
